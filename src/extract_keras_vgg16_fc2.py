dad7fe89771f6a5cf1a2310ef0c5c0aa46f461fe
anhtp <14520040@gm.uit.edu.vn>
2018-01-30 00:19:54 +0700

    fix: Change extracting feature code

--- src/extract_keras_vgg16_fc2.py
+++ src/extract_keras_vgg16_fc2.py
@@ -1,7 +1,9 @@
+""" Extracting vgg16 fc2 using keras framework and tensorflow as backend"""
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 from keras.models import Model
+import sys
 import numpy as np
 import glob
 import tools
@@ -9,7 +11,17 @@ import logging
 import os
 import pickle
 
-def get_fc2(img_path, model):
+def extract_from(img_path, model):
+    """Extract feature from an image
+    
+    Arguments:
+        img_path {string} -- full path to image file
+        model {Model} -- VGG16 model
+    
+    Returns:
+        list -- vector of feature
+    """
+
     img = image.load_img(img_path,target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -19,39 +31,42 @@ def get_fc2(img_path, model):
     fc2_features = model_extractfeatures.predict(x)
     return fc2_features[0]
 
-def get_list_fc2(img_folder, model):
-    list_fc2 = []
-    print('Runing on folder {}...'.format(img_folder))
-    for img_path in glob.glob('{}/*.jpg'.format(img_folder)):
-        print('Extracting feature in from {}...'.format(img_path))
-        list_fc2.append(get_fc2(img_path, model))
-    print('Extracted feature from folder {} success.'.format(img_folder))
-    return list_fc2
+def extract_all(dataset_name, model):
+    """Extract feature for all images in dataset.
+    
+    Arguments:
+        dataset_name {string} -- name of dataset (Ex. 'original')
+        model {Model} -- vgg16 model used to extract feature
+    """
+    logger = logging.getLogger()
+    data_folder = '../data/{}'.format(dataset_name)
+    for dirpath, _, filenames in os.walk(data_folder):
+        for filename in [f for f in filenames if f.endswith(".jpg")]:
+            image_path = os.path.join(dirpath, filename)
+            feature_path = image_path.replace('data', 'bin/features/keras_vgg16_fc2').replace('.jpg', '.pkl')
+            feature_folder = dirpath.replace('data', 'bin/features/keras_vgg16_fc2')
+            if os.path.exists(feature_path):
+                logger.debug('Feature file {} existed.'.format(feature_path))
+            else:
+                if not os.path.exists(feature_folder):
+                    os.makedirs(feature_folder)
+                    logger.info('Running on folder'.format(feature_folder))
+                logger.debug('Extracting from {}...'.format(image_path))
+                feature = extract_from(image_path, model)
+                with open(feature_path, 'wb') as f:
+                    pickle.dump(feature, f)
 
-def get_fc2_and_labels(info_file, model):
-    with open(info_file) as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
-    for line in content:
-        folder = line.split()[0]
-        X_out_file = '../bin/features/keras_vgg16_fc2/{}.pkl'.format(folder)
-        if not os.path.exists(X_out_file):
-            curr_fc2 = get_list_fc2('../data/{}'.format(folder), model)
-            print 'Writing feature to file {}...'.format(X_out_file)
-            with open(X_out_file, 'wb') as f:
-                pickle.dump(curr_fc2, f)
-            print 'Writing success.'        
-        else:
-            print 'File {} existed.'.format(X_out_file)
-
-def main():
+def main(argv):
+    tools.config()
+    if len(argv) < 2:
+        print('Missing arguments: extract_keras_vgg16_fc2.py `dataset_name`. Ex. `python train.py original`')
+        return
     model = VGG16(weights='imagenet', include_top=True)
-    for i in range (1, 6):
-        info_train_file = '../train_test/trainlist0{}.txt'.format(str(i))
-        info_test_file = '../train_test/testlist0{}.txt'.format(str(i))
-        get_fc2_and_labels(info_train_file, model)
-        get_fc2_and_labels(info_test_file, model)
+    dataset_name = argv[1]
+    log_file = '../logs/extract-keras-vgg16-fc2-{}.log'.format(dataset_name)
+    logger = tools.get_logger(log_file, logging.INFO, logging.DEBUG)
+    extract_all(dataset_name, model)
     return 0
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
