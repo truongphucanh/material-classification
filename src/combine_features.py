3e929f615c4c017aef58ddbe2b763a5788580de2
anhtp <14520040@gm.uit.edu.vn>
2018-02-01 18:02:41 +0700

    fix: Change code for combining features

--- src/combine_features.py
+++ src/combine_features.py
@@ -9,29 +9,32 @@ import pickle
 
 def main(argv):
     tools.config()
-    if len(argv) < 4:
-        print('Missing arguments: combine_features.py `feature_name` `dataset_name_1` `dataset_name_2`. \
-            Ex. `python combine_features.py keras_vgg16_fc2 original edges`')
+    if len(argv) < 3:
+        print('Missing arguments: combine_features.py `feature_name1` `feature_name2`. \
+            Ex. `python combine_features.py keras_vgg16_fc2-original keras_vgg16_fc2-edges`')
         return
 
     log_file = '../logs/combine-features.log'
     logger = tools.get_logger(log_file, logging.INFO, logging.DEBUG)
     logger.info('*' * 100)
 
-    feature_name = argv[1]
-    original_feature_folder = '../bin/features/{}/original'.format(feature_name)
+    folder_feature1 = '../bin/features/{}'.format(argv[1])
     combine_feature_name = ''
 
-    for i in range(2, len(argv) - 1):
-        combine_feature_name = combine_feature_name + argv[i] + '-'
+    for i in range(1, len(argv) - 1):
+        combine_feature_name = combine_feature_name + argv[i] + '+'
     combine_feature_name = combine_feature_name + argv[len(argv) - 1]
     logger.info('Combined-feature name: {}'.format(combine_feature_name))
+    logger.debug(folder_feature1)
 
-    for dirpath, _, filenames in os.walk(original_feature_folder):
+    for dirpath, _, filenames in os.walk(folder_feature1):
         for filename in [f for f in filenames if f.endswith(".pkl")]:
-            original_feature_path = os.path.join(dirpath, filename)
-            combined_feature_folder = dirpath.replace('original', combine_feature_name)
-            combined_feature_path = original_feature_path.replace('original', combine_feature_name)
+            if ('train' in filename) or ('test' in filename):
+                logger.debug('skip train test')
+                continue
+            feature1_pkl_path = os.path.join(dirpath, filename)
+            combined_feature_path = feature1_pkl_path.replace(argv[1], combine_feature_name)
+            combined_feature_folder = dirpath.replace(argv[1], combine_feature_name)
             if os.path.exists(combined_feature_path):
                 logger.debug('Combined-feature file {} existed.'.format(combined_feature_path))
             else:
@@ -39,8 +42,8 @@ def main(argv):
                     os.makedirs(combined_feature_folder)
                     logger.info('Running on folder {}'.format(combined_feature_folder))
                 combined_feature = []
-                for i in range(2, len(argv)):
-                    feature_path = original_feature_path.replace('original', argv[i])
+                for i in range(1, len(argv)):
+                    feature_path = feature1_pkl_path.replace(argv[1], argv[i])
                     logger.debug('Extend with feature from {}'.format(feature_path))
                     with open(feature_path, 'rb') as f:
                         combined_feature.extend(pickle.load(f))
