a2fcc73062ac6d28ac810d65dff41123af70b54c
anhtp <14520040@gm.uit.edu.vn>
2018-02-05 16:32:32 +0700

    src: Add dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,64 @@
+# Ex. X, y = dataset.get_data('keras_vgg16_fc2-original', 'trainlist01')
+
+import numpy
+import pickle
+import os
+import logging
+import kit
+
+def get_X(feature_name, split_name):
+    logger = logging.getLogger()
+    logger.debug("Getting X for {} {}.".format(feature_name,  split_name))
+
+    # if feature file existed, use it
+    X_file = '../bin/features/{}/{}.pkl'.format(feature_name, split_name)
+    if os.path.exists(X_file):
+        with open(X_file, 'rb') as fr:
+            X = pickle.load(fr)
+        return numpy.array(X)
+
+    # else, mix feature from single feature files.
+    with open('../splits/{}.txt'.format(split_name)) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    X = []
+    for line in content:
+        folder = line.split()[0]
+        feature_folder = '../bin/features/{}/{}'.format(feature_name, folder)
+        logger.debug('Mix X in folder: {}'.format(feature_folder))
+        for pkl_file in glob.glob('{}/*.pkl'.format(feature_folder)):
+            with open(pkl_file, 'rb') as f:
+                X.append(pickle.load(f))
+    return numpy.array(X)
+
+def get_y(split_name):
+    y_file = '../bin/labels/{}.pkl'.format(split_name)
+    if not os.path.exists(y_file):
+        logger.error('!Error: Label file {} not found.'.format(y_file))
+        return
+    logger.info('Getting y from {}...'.format(y_file))
+    with open(y_file, 'rb') as fr:
+        y = pickle.load(fr)
+    return numpy.array(y)
+
+def get_data(feature_name, split_name):
+    logger = logging.getLogger()
+    logger.info('Getting training data for training set {} with feature {}...'.format(split_name, feature_name))
+    X_file = '../bin/features/{}/{}.pkl'.format(feature_name, split_name)
+    y_file = '../bin/labels/{}.pkl'.format(split_name)
+    
+    # get X
+    X = get_X(feature_name, split_name)
+    logger.info('X shape: {}'.format(numpy.shape(X)))
+
+    # get y
+    y = get_y(split_name)
+    logger.info('y shape: {}'.format(numpy.shape(y)))
+
+    return X, y
+
+def test():
+    logger = kit.get_logger('../logs/dataset.log')
+    X, y = get_data('keras_vgg16_fc2-original', 'trainlist02')
+    print(numpy.shape(X))
+    print(numpy.shape(y))
