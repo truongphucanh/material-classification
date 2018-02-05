815ca57acfa6e2a5c3ecfdd31c8ae15554815441
anhtp <14520040@gm.uit.edu.vn>
2018-02-05 17:11:10 +0700

    src: Edit src/models.py

--- src/models.py
+++ src/models.py
@@ -1,18 +1,16 @@
 # models, lk, lc, ld, lg, lp = models.getmodels('../config/models_config.csv')
+from sklearn import svm
 import numpy
 import os
 import csv
-import logging
 import kit
-from sklearn import svm
 
-def get_models(config_file):
-    logger = logging.getLogger()
+def get_models(config_file='../config/models_config.csv'):
     if not config_file.endswith('.csv'):
-        logger.error('!Error: models config file must be *.csv, but it is {}'.format(config_file))
+        print('!Error: models config file must be *.csv, but it is {}'.format(config_file))
         return
     if not os.path.exists(config_file):
-        logger.error('!Error: File not found {}'.format(config_file))
+        print('!Error: File not found {}'.format(config_file))
         return
     models = []
     list_kernel = []
@@ -26,10 +24,10 @@ def get_models(config_file):
         isFirstRow = True
         for row in rows:
             if len(row) < 5:
-                logger.error('!Error: Invalid format in models config file {}. Please folow format \"kernel, C, degree, gamma\"'.format(config_file))
+                print('!Error: Invalid format in models config file {}. Please folow format \"kernel, C, degree, gamma\"'.format(config_file))
                 return
             if isFirstRow:
-                logger.debug('Skip first row')
+                print('Skip first row')
                 isFirstRow = False
                 continue
             kernel = row[0]
@@ -47,7 +45,7 @@ def get_models(config_file):
             elif kernel == 'rbf':
                 model = svm.SVC(kernel=kernel, C=float(C), gamma=float(gamma), probability=kit.str2bool(prob))
             else:
-                logger.error('!Error: Invalid kernel. Kernel must be one of `linear, rbf or poly` but it is {}').format(kernel)
+                print('!Error: Invalid kernel. Kernel must be one of `linear, rbf or poly` but it is {}').format(kernel)
             models.append(model)
             list_kernel.append(kernel)
             list_C.append(C)
@@ -57,7 +55,6 @@ def get_models(config_file):
     return numpy.array(models), numpy.array(list_kernel), numpy.array(list_C), numpy.array(list_degree), numpy.array(list_gamma), numpy.array(list_prob)
 
 def confirm():
-    logger = kit.get_logger('../logs/models.log')
     models, lk, lc, ld, lg, lp = get_models('../config/models_config.csv')
     print(lk, lc, ld, lg, lp)
 
