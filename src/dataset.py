dc9e2c25164b8765cd9b7ad706ca40302a3c7525
anhtp <14520040@gm.uit.edu.vn>
2018-03-20 22:55:27 +0700

    fix: Edit ./src/train.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,62 +1,43 @@
-# Ex. X, y = dataset.get_data('keras_vgg16_fc2-original', 'trainlist01')
+# Ex. X, y = dataset.get_data("keras_vgg16_fc2-original", "trainlist01")
 
 import numpy
 import pickle
 import os
 import glob
 
-def get_X(feature_name, split_name):
-    print("Getting X for {} {}.".format(feature_name,  split_name))
+def get_data(dataset_name, feature_type, split_name):
+    print("Get X. dataset_name: {}, feautre_type: {}, split_name: {}".format(dataset_name, feature_type, split_name))
 
     # if feature file existed, use it
-    X_file = '../bin/features/{}/{}.pkl'.format(feature_name, split_name)
-    if os.path.exists(X_file):
-        with open(X_file, 'rb') as fr:
+    X_file = "./DS_{}/result/features/{}/{}.pkl".format(dataset_name, feature_type, split_name)
+    y_file = "./DS_{}/result/labels/{}/{}.pkl".format(dataset_name, feature_type, split_name)
+    if os.path.exists(X_file) and os.path.exists(y_file):
+        with open(X_file, "rb") as fr:
             X = pickle.load(fr)
-        return numpy.array(X)
+        with open(y_file, "rb") as fr:
+            y = pickle.load(fr)
+        return numpy.array(X), numpy.array(y)
 
     # else, mix feature from single feature files.
-    with open('../splits/{}.txt'.format(split_name)) as f:
+    split_file = "./DS_{}/splits/{}.txt".format(dataset_name, split_name)
+    with open(split_file) as f:
         content = f.readlines()
     content = [x.strip() for x in content]
     X = []
+    y = []
     for line in content:
-        folder = line.split()[0]
-        feature_folder = '../bin/features/{}/{}'.format(feature_name, folder)
-        print('Mix X in folder: {}'.format(feature_folder))
-        for pkl_file in glob.glob('{}/*.pkl'.format(feature_folder)):
-            with open(pkl_file, 'rb') as f:
-                X.append(pickle.load(f))
-    return numpy.array(X)
-
-def get_y(split_name):
-    y_file = '../bin/labels/{}.pkl'.format(split_name)
-    if not os.path.exists(y_file):
-        print('!Error: Label file {} not found.'.format(y_file))
-        return
-    print('Getting y from {}...'.format(y_file))
-    with open(y_file, 'rb') as fr:
-        y = pickle.load(fr)
-    return numpy.array(y)
-
-def get_data(feature_name, split_name):
-    print('Getting training data for training set {} with feature {}...'.format(split_name, feature_name))
-    X_file = '../bin/features/{}/{}.pkl'.format(feature_name, split_name)
-    y_file = '../bin/labels/{}.pkl'.format(split_name)
-    
-    # get X
-    X = get_X(feature_name, split_name)
-    print('X shape: {}'.format(numpy.shape(X)))
-
-    # get y
-    y = get_y(split_name)
-    print('y shape: {}'.format(numpy.shape(y)))
-
-    return X, y
+        jpg_file = line.split()[0]
+        label = line.split()[1]
+        pkl_file = jpg_file.replace("data", "result/features").replace("jpg", "pkl")
+        print("Read file {}".format(pkl_file))
+        with open(pkl_file, "rb") as f:
+            X.append(pickle.load(f))
+            y.append(label)
+    return numpy.array(X), numpy.array(y)
 
 def confirm():
-    X, y = get_data('keras_vgg16_fc2-original', 'trainlist02')
+    X, y = get_data("FMD", "original", "train_split_1")
     print(numpy.shape(X))
     print(numpy.shape(y))
 
-# confirm()
+#confirm()
--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
-# Example: python train.py keras_vgg16_fc2-original 1 1
+# Format:   python ./src/train.py <dataset_name> <feature_type> <trainsplit_index_low> <trainsplit_index_high>
+# Example:  python ./src/train.py FMD original 1 1
+
 from sklearn.externals import joblib
 import pickle
 import sys
@@ -13,82 +15,71 @@ from mkit import mlog
 
 OVERWRITE = False
 
-def train(feature_name, split_name):
-    """Train for a training set from ./train_test folder with specific feature_name
-    
-    Arguments:
-        feature_name {string} -- feature_name (Ex. 'keras_vgg16_fc2').
-        split_name {string} -- split name (Ex. 'trainlist001').
-    """
+def train(dataset_name, feature_type, split_name):
     # get logger
     logger = logging.getLogger()
-    logger.info('Start training for '.format(feature_name, split_name))
+    logger.info("Start training: DS_{}, feature {}, split {}".format(dataset_name, feature_type, split_name))
 
     # get models
-    clfs, lkernel, lC, ld, lg = models.get_models('../config/models_config.csv')
+    clfs, lkernel, lC, ld, lg = models.get_models("./config/models_config.csv")
 
     # get data
-    feature_dir = '../bin/features/{}'.format(feature_name)
+    feature_dir = "./DS_{}/result/features/{}".format(dataset_name, feature_type)
     if not os.path.exists(feature_dir):
-        logger.error('!Error: Feature folder {} not found. Please run feature extractor for this feature'.format(feature_dir))
+        logger.error("!Error: Feature folder {} not found".format(feature_dir))
         return
-    X, y = dataset.get_data(feature_name, split_name)
+    X, y = dataset.get_data(dataset_name, feature_type, split_name)
 
     # fit data and save model to file
-    models_dir = '../bin/models/{}/{}'.format(feature_name, split_name)
-    fittime_file = '{}/time.csv'.format(models_dir)
+    models_dir = "./DS_{}/result/models/{}/{}".format(dataset_name, feature_type, split_name)
+    fittime_file = "{}/time.csv".format(models_dir)
     if not os.path.exists(models_dir):
-        logger.info('Creating models folder {}...'.format(models_dir))
+        logger.info("Creating models folder {}...".format(models_dir))
         os.makedirs(models_dir)
     if OVERWRITE:
-        with open(fittime_file, 'wb') as fw:
-            fw.write('Kernel,C,degree,gamma,fit time\n')
+        with open(fittime_file, "wb") as fw:
+            fw.write("Kernel,C,degree,gamma,fit time\n")
     for i, clf in enumerate(clfs):
-        model_file = '{}_{}_{}_{}.pkl'.format(lkernel[i], lC[i], ld[i], lg[i])
-        model_dir = '{}/{}'.format(models_dir, model_file)
+        model_file = "{}_{}_{}_{}.pkl".format(lkernel[i], lC[i], ld[i], lg[i])
+        model_dir = "{}/{}".format(models_dir, model_file)
         if (not OVERWRITE) and os.path.exists(model_dir):
-            logger.info('Model {} existed.'.format(model_dir))
+            logger.info("Model {} existed.".format(model_dir))
             continue
-        logger.info('Fitting with model {}: {}'.format(i, model_file))
+        logger.info("Fitting with model {}: {}".format(i, model_file))
         start_time = time.time()
         clf.fit(X, y)
         fit_time = time.time() - start_time
-        logger.info('Done. fit_time = {}'.format(fit_time))
-        logger.info('Saving model {} to {} ...'.format(i, models_dir))
+        logger.info("Done. fit_time = {}".format(fit_time))
+        logger.info("Saving model {} to {} ...".format(i, models_dir))
         joblib.dump(clf, model_dir)
-        with open(fittime_file, 'a') as fw:
-            fw.write('{},{},{},{},{}\n'.format(lkernel[i], lC[i], ld[i], lg[i], fit_time))
+        with open(fittime_file, "a") as fw:
+            fw.write("{},{},{},{},{}\n".format(lkernel[i], lC[i], ld[i], lg[i], fit_time))
 
-def train_for(feature_name, low, high):
-    """Train for all train set in ./train_test folder with specific feature name
-    
-    Arguments:
-        feature_name {string} -- feature name (Ex. 'keras_vgg16_fc2-original')
-    """
+def train_for(dataset_name, feature_type, low, high):
     logger = logging.getLogger()
-    feature_dir = '../bin/features/{}'.format(feature_name)
+    feature_dir = "./DS_{}/result/features/{}".format(dataset_name, feature_type)
     if not os.path.exists(feature_dir):
-        logger.error('!Error: Feature folder {} not found. Please run feature extractor for this feature'.format(feature_dir))
+        logger.error("!Error: Feature folder {} not found".format(feature_dir))
         return
     for i in range(low, high + 1):
-        logger.info('>'*100)
-        train(feature_name, 'trainlist0{}'.format(i))
+        train(dataset_name, feature_type, "train_split_{}".format(i))
 
 def main(argv):
     mlog.config()
-    logger = mlog.get_logger('../logs/train.log')
-    if len(argv) <= 1:
-        print('Missing arguments. Please try again.')
-        print('Format: python train.py {feature name}')
-        print('Example: python train.py keras_vgg16_fc2-original')
+    logger = mlog.get_logger("./logs/train.log")
+    if len(argv) < 3:
+        print("Missing arguments. Please try again.")
+        print("# Format:   python ./src/train.py <dataset_name> <feature_type> <trainsplit_index_low> <trainsplit_index_high>")
+        print("# Example:  python ./src/train.py FMD original 1 1")
         return
-    feature_name = argv[1]
+    dataset_name = argv[1]
+    feature_type = argv[2]
     LOW_INDEX = 1
     HIGH_INDEX = 5
     if len(argv) >= 4:
-        LOW_INDEX = int(argv[2])
-        HIGH_INDEX = int(argv[3])
-    train_for(feature_name, LOW_INDEX, HIGH_INDEX)
+        LOW_INDEX = int(argv[3])
+        HIGH_INDEX = int(argv[4])
+    train_for(dataset_name, feature_type, LOW_INDEX, HIGH_INDEX)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv)
