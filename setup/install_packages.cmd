0e40869f866d005f38471e3e046188152750aa34
Truong Phuc Anh <14520040@gm.uit.edu.vn>
2018-06-03 17:57:00 +0700

    Added open cv installation

--- setup/install_packages.cmd
+++ setup/install_packages.cmd
@@ -6,6 +6,8 @@ pip3 install keras
 pip3 install scikit-learn
 pip3 install h5py
 pip3 install pillow
+pip3 install opencv-python
+pip3 install opencv-contrib-python --upgrade --user
 
 echo </install python's packages>
 pause
